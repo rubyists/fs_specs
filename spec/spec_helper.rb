d643b996a7bc27612f32e2a37094de1b1a957c6b
Deryl R. Doucette <me@deryldoucette.com>
2012-02-08 19:42:35 -0600

    Bit of RSpec cleanup for deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-require './helpers'
+require File.dirname(__FILE__) + "/helpers"
 
 require 'yaml'
 require 'rspec/autorun'
 
 require File.dirname(__FILE__) + "/custom_matchers"
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include(CustomMatcher)
 end
 
