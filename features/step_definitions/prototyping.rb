02d6e2d991a7f357a7b4896695a28a587e1d8875
Deryl R. Doucette <me@deryldoucette.com>
2012-02-14 19:24:08 -0600

    Minor change to forcea travis resubmit for the last commit because ruby2 worker needed its VMs rebuilt. So the last test died before it sprang to life.

--- features/step_definitions/prototyping.rb
+++ features/step_definitions/prototyping.rb
@@ -1,7 +1,6 @@
 Then /^I should be able to build a prototype$/ do
   EM.run do
-    # Wait 60 seconds
+    #  Wait 60 seconds
     EM.add_periodic_timer(60) { |e| EM.stop }
-    EM.connect(@server2, 8021, SimulAgentListener, @sock1, @sock2, @server1, @server2, known_extension = "9192")
   end
 end
