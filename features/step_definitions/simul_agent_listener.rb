c56b4397893545c96ceca32ca90f00e14710afa6
Deryl R. Doucette <me@deryldoucette.com>
2012-02-15 11:01:54 -0600

    OK, now we're getting everything I thought we should be getting. I left the internal EM.add_periodic_timer(5) and closing end in the class definition, but commented out. If we need to, we can reactivate it and cull from within. My only question right now is why I'm starting out the entire chain with CHANNEL_EXECUTE_COMPLETE every time. Once CHANNEL_CALLSTATE is reached the last time (when event.content[:channel_call_state] == DOWN), that closes that session. Are we getting part of the startup sequence's (for that listener) CHANNEL_* entries? If so then why am I not seeing them since the caller_id_number should be the same as the @spec_id and we're culling ONLY matching @spec_id events.

--- features/step_definitions/simul_agent_listener.rb
+++ features/step_definitions/simul_agent_listener.rb
@@ -51,55 +51,56 @@ WANTED_STATE ={
     #
     # Make sure we *only* process this specific spec's events
     return unless (event.content[:caller_caller_id_number] == @spec_id)
-    return "NOT a json'd event!" unless (event.headers[:content_type] == "text/event-json")
+    #EM.add_periodic_timer(5) do
+      return "NOT a json'd event!" unless (event.headers[:content_type] == "text/event-json")
 
-    # This is what we really want to see
-    if WANTED_STATE[:event_name] == "#{event.content[:event_name]}" && WANTED_STATE[:channel_state] == "#{event.content[:channel_state]}" && WANTED_STATE[:call_state] == "#{event.content[:channel_call_state]}"
+      # This is what we really want to see
+      if WANTED_STATE[:event_name] == "#{event.content[:event_name]}" && WANTED_STATE[:channel_state] == "#{event.content[:channel_state]}" && WANTED_STATE[:call_state] == "#{event.content[:channel_call_state]}"
 
-      # Got what we wanted, so do your 'something'
-      puts "Event we're handing is: #{event.content[:event_name]}"
-      puts "GOT CHANNEL STATE - #{WANTED_STATE[:channel_state]}"
-      puts "GOT CALL STATE == #{WANTED_STATE[:call_state]}"
-      puts "WANTED_STATE triggered this. We can do fill_in_blank using just these:"
-      puts "Caller #{event.content[:caller_channel_name]} dialed extension #{event.content[:caller_destination_number]} in the #{event.content[:caller_dialplan]} dialplan on #{event.content[:freeswitch_switchname]}."
-      puts
-      puts "event.content[:event_name] == #{event.content[:event_name]} | spec_id == #{@spec_id}"
-      puts "event.content[:unique_id] == #{event.content[:unique_id]}"
-      puts "event.content[:event_calling_file] == #{event.content[:event_calling_file]} | spec_id == #{@spec_id}"
-      puts "event.content[:event_calling_function] == #{event.content[:event_calling_function]} | spec_id == #{@spec_id}"
-      puts "event.content[:channel_name] == #{event.content[:channel_name]}"
-      puts "event.content[:channel_state] == #{event.content[:channel_state]} | spec_id == #{@spec_id}"
-      puts "event.content[:channel_call_state] == #{event.content[:channel_call_state]} | spec_id == #{@spec_id}"
-      puts "event.content[:channel_hit_dialplan] == #{event.content[:channel_hit_dialplan]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_channel_name] == #{event.content[:caller_channel_name]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_unique_id] == #{event.content[:caller_unique_id]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_source] == #{event.content[:caller_source]}"
-      puts "event.content[:caller_network_addr] == #{event.content[:caller_network_addr]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_caller_id_number] == #{event.content[:caller_caller_id_number]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_direction] == #{event.content[:caller_direction]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_context] == #{event.content[:caller_context]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_dialplan] == #{event.content[:caller_dialplan]} | spec_id == #{@spec_id}"
-      puts "event.content[:caller_destination_number] == #{event.content[:caller_destination_number]} | spec_id == #{@spec_id}"
-      puts "event.content[:variable_current_application] == #{event.content[:variable_current_application]}"
-      puts "event.content[:variable_originate_disposition] == #{event.content[:variable_originate_disposition]}"
-      puts "event.content[:variable_endpoint_disposition] == #{event.content[:variable_endpoint_disposition]}"
-      puts "event.content[:freeswitch_switchname] == #{event.content[:freeswitch_switchname]} | spec_id == #{@spec_id}"
-      puts "= ENDS WANTED_STATE ="
-    else
-      puts "Got different than WANTED_STATE. Processing different event than we thought. We got: "
-      puts "event.content[:event_name] == #{event.content[:event_name]} - event.content[:channel_state] == #{event.content[:channel_state]}  - event.content[:channel_call_state] == #{event.content[:channel_call_state]}"
-      puts "event.content[:variable_originate_disposition] == #{event.content[:variable_originate_disposition]}"
-      puts "event.content[:variable_endpoint_disposition] == #{event.content[:variable_endpoint_disposition]}"
-    end
-
+        # Got what we wanted, so do your 'something'
+        puts "Event we're handing is: #{event.content[:event_name]}"
+        puts "GOT CHANNEL STATE - #{WANTED_STATE[:channel_state]}"
+        puts "GOT CALL STATE == #{WANTED_STATE[:call_state]}"
+        puts "WANTED_STATE triggered this. We can do fill_in_blank using just these:"
+        puts "Caller #{event.content[:caller_channel_name]} dialed extension #{event.content[:caller_destination_number]} in the #{event.content[:caller_dialplan]} dialplan on #{event.content[:freeswitch_switchname]}."
+        puts
+        puts "event.content[:event_name] == #{event.content[:event_name]} | spec_id == #{@spec_id}"
+        puts "event.content[:unique_id] == #{event.content[:unique_id]}"
+        puts "event.content[:event_calling_file] == #{event.content[:event_calling_file]} | spec_id == #{@spec_id}"
+        puts "event.content[:event_calling_function] == #{event.content[:event_calling_function]} | spec_id == #{@spec_id}"
+        puts "event.content[:channel_name] == #{event.content[:channel_name]}"
+        puts "event.content[:channel_state] == #{event.content[:channel_state]} | spec_id == #{@spec_id}"
+        puts "event.content[:channel_call_state] == #{event.content[:channel_call_state]} | spec_id == #{@spec_id}"
+        puts "event.content[:channel_hit_dialplan] == #{event.content[:channel_hit_dialplan]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_channel_name] == #{event.content[:caller_channel_name]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_unique_id] == #{event.content[:caller_unique_id]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_source] == #{event.content[:caller_source]}"
+        puts "event.content[:caller_network_addr] == #{event.content[:caller_network_addr]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_caller_id_number] == #{event.content[:caller_caller_id_number]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_direction] == #{event.content[:caller_direction]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_context] == #{event.content[:caller_context]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_dialplan] == #{event.content[:caller_dialplan]} | spec_id == #{@spec_id}"
+        puts "event.content[:caller_destination_number] == #{event.content[:caller_destination_number]} | spec_id == #{@spec_id}"
+        puts "event.content[:variable_current_application] == #{event.content[:variable_current_application]}"
+        puts "event.content[:variable_originate_disposition] == #{event.content[:variable_originate_disposition]}"
+        puts "event.content[:variable_endpoint_disposition] == #{event.content[:variable_endpoint_disposition]}"
+        puts "event.content[:freeswitch_switchname] == #{event.content[:freeswitch_switchname]} | spec_id == #{@spec_id}"
+        puts "= ENDS WANTED_STATE ="
+      else
+        puts "Got different than WANTED_STATE. Processing different event than we thought. We got: "
+        puts "event.content[:event_name] == #{event.content[:event_name]} - event.content[:channel_state] == #{event.content[:channel_state]}  - event.content[:channel_call_state] == #{event.content[:channel_call_state]}"
+        puts "event.content[:variable_originate_disposition] == #{event.content[:variable_originate_disposition]}"
+        puts "event.content[:variable_endpoint_disposition] == #{event.content[:variable_endpoint_disposition]}"
+      end
+    #end
     #pp event.content
     # We check if event.content exists. if it dont we're in deep doodoo. only using check to shut down the reactor
-    if event.content
+    #if event.content
       # And hang up the call
-      @sock1.kill(@uuid).run
+    #  @sock1.kill(@uuid).run
       # Then stop the reactor
-      EM.stop
-    end
+    #  EM.stop
+    #end
   end
 
   def unbind
